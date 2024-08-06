962670bf05f4642ac1e4fcc2b5a9c029e80b67b4
Kamal Chaudhari <kamalchaudhary502@gmail.com>
2024-08-07 00:17:06 +0530

    Using Feign client to call Microservices

---
+++ src/main/java/com/project/microservices/currency_conversion_service/Proxy/CurrencyExchangeProxy.java
@@ -0,0 +1,16 @@
+package com.project.microservices.currency_conversion_service.Proxy;
+
+import com.project.microservices.currency_conversion_service.Beans.CurrencyConversion;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "currency-exchange" ,url = "localhost:8000")
+public interface CurrencyExchangeProxy {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversion retrieveExchangeValue(
+            @PathVariable String from,
+            @PathVariable String to);
+
+}
